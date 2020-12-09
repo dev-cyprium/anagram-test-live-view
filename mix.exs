424be1f0f8a12efb93fa0f8491261669a6a32e16
Stefan Kupresak <stefan_vg@hotmail.com>
2020-12-10 00:55:59 +0100

    add travis, coveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,9 @@ defmodule AnagramApi.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -43,7 +45,8 @@ defmodule AnagramApi.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_live_view, "~> 0.14.6"},
-      {:floki, ">= 0.27.0", only: :test}
+      {:floki, ">= 0.27.0", only: :test},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 
