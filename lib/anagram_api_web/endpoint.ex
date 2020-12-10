6583be6fbca51e20f4c1a4fc5ea3c50dcaae93b5
Stefan Kupresak <stefan_vg@hotmail.com>
2020-12-10 01:53:45 +0100

    prepare app to deploy to Heroku

--- lib/anagram_api_web/endpoint.ex
+++ lib/anagram_api_web/endpoint.ex
@@ -14,7 +14,7 @@ defmodule AnagramApiWeb.Endpoint do
     websocket: true,
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options], timeout: 45_000]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
